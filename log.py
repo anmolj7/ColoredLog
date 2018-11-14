ece848791a1da761c89d0d8901b3578fa72ac0c4
Anmol Jhamb <30911149+anmolj7@users.noreply.github.com>
2018-11-15 01:31:45 +0530

    Add files via upload

---
+++ log.py
@@ -0,0 +1,21 @@
+import logging
+import coloredlogs
+coloredlogs.install()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+fh = logging.FileHandler(r'log.txt')
+fh.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
+
+logger.debug('debug message')
+logger.info('info message')
+logger.warn('warn message')
+logger.error('error message')
+logger.critical('critical message')
+
